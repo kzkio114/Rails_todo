c802467b62625342eedaa05854f15c6e429abeeb
kzkio114 <kzkio114@gmail.com>
2024-11-22 23:49:28 +0900

    ルート等追加

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,8 @@
 class TopsController < ApplicationController
   def index
   end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # get "tops/index"
   root "tops#index"
+  post "todo" => "tops/todo" , as: :todo
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
