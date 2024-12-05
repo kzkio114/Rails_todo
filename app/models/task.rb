9769c628de9c0227a7db78fdcc624fb4b3b05539
kzkio114 <kzkio114@gmail.com>
2024-12-06 05:46:31 +0900

    jobでリアルタイム完了

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ApplicationRecord
-  broadcasts_to ->(task) { "tasks_list" }, inserts_by: :append
+  broadcasts_to ->(task) { "tasks_list" }, inserts_by: :append, target: "tasks_list"
 end
