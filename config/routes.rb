b7a1701bd380293a03ea8d0681ce132962061ce0
kzkio114 <kzkio114@gmail.com>
2024-12-05 17:34:59 +0900

    編集の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tasks, only: [ :index, :show, :create, :update, :destroy ]
+  resources :tasks, only: [ :index, :edit, :show, :create, :update, :destroy ]
   root "tops#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
