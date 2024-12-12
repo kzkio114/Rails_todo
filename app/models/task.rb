7964823f47ac219e1ec9f01fbb3ddd9259213a1f
kzkio114 <kzkio114@gmail.com>
2024-12-13 02:19:38 +0900

    タスク関係を追記

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,20 +1,11 @@
 class Task < ApplicationRecord
   broadcasts_to ->(task) { "tasks_list" }, inserts_by: :append, target: "tasks_list"
-  
+
   after_destroy_commit -> {
     broadcast_replace_to(
       "task_detail",
       target: "task_detail",
       partial: "tasks/detail_deleted",
-      locals: { task: self }
-    )
-  }
-
-  after_update_commit -> {
-    broadcast_morph_to(
-      "task_preview",
-      target: "task_preview",
-      partial: "tasks/preview",
       locals: { task: self }
     )
   }
